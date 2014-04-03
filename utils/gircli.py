afbe9d3afbde7ec29a884a894ba65f66e1a864f3
zysh <hi.imma.bear@gmail.com>
2014-04-03 02:03:25 -0700

    Updated readme, command line tool updated

--- utils/gircli.py
+++ utils/gircli.py
@@ -2,28 +2,45 @@
 import argparse
 import socket
 import sys
+import os
 
 SLAVE = '169.254.159.73'
 PORT = 23
 
-def create_parser():
+def parse_args():
     parser = argparse.ArgumentParser('A simple CLI tool for interacting with Gir.')
     parser.add_argument('-d', '--development', action = 'store_true', help = 'Uses the development server')
-    parser.add_argument('command', metavar = 'CMD', required = True, nargs = '+', help = 'Command to send to Gir')
-    return parser
+    parser.add_argument('command', metavar = 'CMD',  nargs = '+', help = 'Command to send to Gir')
+    return parser.parse_args()
 
 def main():
-    parser = create_parser()
-    """
-    cmd = " ".join(sys.argv[1:]) + '\r'
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((SLAVE, PORT))
-    s.sendall(cmd)
-    data = s.recv(1024)
-    print data
+    args = parse_args()
 
-    s.close()"""
-    print parser.parse_args()
+    if args.command is None:
+        print "Error: An empty or nil command can't be sent, giving up."
+        sys.exit(1)
+
+    if args.development:
+        SLAVE = ''
+        PORT = 50000
+        if os.system('nc -z localhost 50000') is None:
+            print 'The development server is not running, please run girserver and try again.'
+            sys.exit(1)
+
+    cmd = ' '.join(args.command) + '\r'
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((SLAVE, PORT))
+        s.sendall(cmd)
+        data = s.recv(1024)
+        print data
+        s.close()
+    except:
+        print 'A connection could not be made to the designated server, giving up.'
+        s.close()
+        sys.exit(1)
+
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
